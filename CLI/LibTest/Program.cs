00a19755c5d81d29ce74600da395651958fefe6d
Clover <sudomrclover@gmail.com>
2021-08-06 19:26:58 -0300

    Remove some old code and add a new MusicEntry class

---
+++ CLI/LibTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DonDonLibrary.IO;
+using DonDonLibrary.Database.Vita;
+
+namespace LibTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(EndianBinaryReader r = new EndianBinaryReader(File.Open(args[0], FileMode.Open), Endianness.LittleEndian))
+            {
+                VitaMusicInfo.Read(r);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ DonDonLibrary/Database/MusicEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace DonDonLibrary.Database
+{
+    // I would implement entry reading inside a MusicEntry class
+    // but the names are separate from the main entry so it would just be an overcomplicated code
+    public struct MusicEntry
+    {
+        public string name;
+        public int offset;
+        public int previewStart;
+    }
+}
